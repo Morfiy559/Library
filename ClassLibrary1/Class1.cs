3a50036c202778f562012c40c92863df41ac3af0
Morfiy559 <61278544+Morfiy559@users.noreply.github.com>
2021-03-07 12:08:47 +0300

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void SumFunc(double x)
+        {
+            double Area = Math.Pow(x,2) * Math.PI;
+            Console.WriteLine("Summ = {0}", Area);
+            Console.ReadLine();
+        }
+        public static void SumFunc(double A, double B, double C)
+        {
+            double p, S;
+            p = (A + B + C) / 2;
+
+            S = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            Console.WriteLine("Площадь равна: {0}", S);
+            Console.ReadLine();
+        }
+    }
+}
